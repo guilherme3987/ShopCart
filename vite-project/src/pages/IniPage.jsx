fd9ce6be22b198b52fcada93a458d8ca90c924af
guilherme3987 <guilhermesmt3987@gmail.com>
2024-10-17 19:02:15 -0300

    Criando componente logo

--- vite-project/src/pages/IniPage.jsx
+++ vite-project/src/pages/IniPage.jsx
@@ -1,6 +1,9 @@
+import '../styles/IniPage.css';
+
 export default function IniPage() {
     return(
         <>
+            <div id="logo"></div>
             <h1>Shop Cart</h1>
         </>
     )
