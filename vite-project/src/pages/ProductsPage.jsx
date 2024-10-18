4bc766fc9beaaffa551490adaa819873989904ef
guilherme3987 <guilhermesmt3987@gmail.com>
2024-10-18 16:36:19 -0300

    função ProductsPage

--- vite-project/src/pages/ProductsPage.jsx
+++ vite-project/src/pages/ProductsPage.jsx
@@ -1,8 +1,18 @@
 import '../styles/ProductsPage.css';
+import React, { useEffect, useState } from 'react';
 
 export default function ProductsPage() {
+    
+    {/**Depois tem que implementar a lógica para tratar a API */}
+    const [products, setProducts] = useState([])
+    const [loading, setLoading] = useState(true)
+    const [error, setError] = useState(null)
+    
+    //fetch async
+
     return(
         <>
+           
             <h1>Página de Produtos</h1>
 
             <div id="box-products">
