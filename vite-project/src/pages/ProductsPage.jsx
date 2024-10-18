fae4ca757de2eedaf0f8becc8e5dabff3b8c4db5
guilherme3987 <guilhermesmt3987@gmail.com>
2024-10-18 16:07:54 -0300

    página de produtos e estilização teste

--- vite-project/src/pages/ProductsPage.jsx
+++ vite-project/src/pages/ProductsPage.jsx
@@ -1,7 +1,24 @@
+import '../styles/ProductsPage.css';
+
 export default function ProductsPage() {
     return(
         <>
             <h1>Página de Produtos</h1>
+
+            <div id="box-products">
+                <div class="product-item"></div>
+
+                <div class="product-item"></div>
+
+                <div class="product-item"></div>
+
+                <div class="product-item"></div>
+
+                <div class="product-item"></div>
+
+                <div class="product-item"></div>
+            </div>
+
         </>
     )
 }
