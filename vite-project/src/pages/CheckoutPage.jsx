fad71dc123e2b24b54cb5737584d7b744285bcf9
guilherme3987 <guilhermesmt3987@gmail.com>
2024-10-24 18:08:05 -0300

    botão de compra

--- vite-project/src/pages/CheckoutPage.jsx
+++ vite-project/src/pages/CheckoutPage.jsx
@@ -4,6 +4,8 @@ export default function CheckoutPage(){
     return(
         <>
             <h1>Página de carrinhos de produtos</h1>
+            <button div="buy">Comprar</button>
+
         </>
     )
 }
